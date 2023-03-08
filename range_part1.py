4608f8b4e130e39555911571765da6a02463154a
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-03-08 20:39:28 +0530

    Add files via upload

---
+++ range_part1.py
@@ -0,0 +1,40 @@
+# a=1,2,3,4,5
+# print(*a)
+
+# a=range(10)
+# print(a)
+
+# a=10,20,40
+# for ar in a:
+#     print(ar)
+
+# a=10,20,40
+# for p in a:
+#     print(p,end=' ')
+
+# for i in range(5):
+#     print(i)
+
+# n=int(input("Enter the value of n:"))
+# for i in range(1,n+1):
+#     print(i,end="\n")
+
+# for i in range(1,30,2):
+    # print(i,end=' ')
+
+# for i in range(2,30,2):
+    # print(i,end=' ')
+
+# for i in range(10,0,-1):
+    # print(i,end=' ')
+
+# for i in range(10,-1,-1):
+    # print(i,end=' ')
+
+# n=[1,4.5,'python']
+# for i in n[::-1]:
+#     print(i)
+
+# v='programming'
+# for e in v[::2]:
+#     print(e)
